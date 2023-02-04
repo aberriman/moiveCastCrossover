85cc36a55050885104b5326de44f643876d6245f
aberriman <124303981+aberriman@users.noreply.github.com>
2023-02-04 20:33:16 +0000

    change to streamlit

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-import argparse
-from utils.imdb_request import get_movie_id, get_cast_list, union_of_casts
+import streamlit as st
+
+from utils.imdb_request import get_movie_id, get_cast_list, intersect_of_casts, make_actor_dict
 
 
-def cli_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--movie1", type=str, help="The full name of the first movie")
-    parser.add_argument("--movie2", type=str, help="The full name of the second movie")
-    parser.add_argument("--key", type=str, help="Your API key for IMDB")
-    return parser.parse_args()
+def main():
+    st.text_input("Movie 1", key="movie_id1")
+    st.text_input("Movie 1", key="movie_id2")
+    st.text_input("IMDB API key", key="key")
 
+    movie1 = st.session_state.movie_id1
+    movie2 = st.session_state.movie_id2
+    key = st.session_state.key
 
-def main(movie1, movie2, key):
-    movie1_id = get_movie_id(movie1, key)
-    movie2_id = get_movie_id(movie2, key)
+    if st.button("search"):
+        movie1_id = get_movie_id(movie1, key)
+        movie2_id = get_movie_id(movie2, key)
 
-    movie_cast_1 = get_cast_list(movie1_id, key)
-    movie_cast_2 = get_cast_list(movie2_id, key)
+        cast_list_dict_1, movie_cast_1 = get_cast_list(movie1_id, key)
+        cast_list_dict_2, movie_cast_2 = get_cast_list(movie2_id, key)
 
-    intersect = union_of_casts(movie_cast_1, movie_cast_2)
-    print(intersect)
+        intersect = intersect_of_casts(movie_cast_1, movie_cast_2)
+        st.write(intersect)
+
+        actor_information = make_actor_dict(intersect, cast_list_dict_1, cast_list_dict_2)
+        st.write(actor_information)
 
 
 if __name__ == '__main__':
-    args = cli_args()
-    main(args.movie1, args.movie2, args.key)
+    main()
--- utils/imdb_request.py
+++ utils/imdb_request.py
@@ -11,10 +11,26 @@ def get_movie_id(movie, key):
 def get_cast_list(movie_id, key):
     url = f"https://imdb-api.com/en/API/FullCast/{key}/{movie_id}"
     r = requests.get(url)
-    cast_list_dicts = r.json()["actors"]
-    cast_list = [item["name"] for item in cast_list_dicts]
-    return cast_list
+    cast_list_dict = r.json()["actors"]
+    cast_list = [item["name"] for item in cast_list_dict]
+    return cast_list_dict, cast_list
 
 
-def union_of_casts(movie_cast_1, movie_cast_2):
+def intersect_of_casts(movie_cast_1, movie_cast_2):
     return list(set(movie_cast_1) & set(movie_cast_2))
+
+
+def make_actor_dict(intersection, movie_dict_1, movie_dict_2):
+    actors = dict()
+    for cast_member in movie_dict_1:
+        if cast_member["name"] in intersection:
+            actors[cast_member["name"]] = dict()
+            actors[cast_member["name"]]["image"] = cast_member["image"]
+            actors[cast_member["name"]]["Character_1"] = cast_member["asCharacter"]
+
+    for cast_member in movie_dict_2:
+        if cast_member["name"] in intersection:
+            actors[cast_member["name"]]["Character_2"] = cast_member["asCharacter"]
+
+    return actors
+
